3f0dc007d1bef1ad25be5d0d7b8bd679e4ddfcd7
Steve Kao <steve.kao@lootcrate.com>
2015-04-09 14:38:36 -0700

    TES-65 #resolve

---
+++ test.js
@@ -0,0 +1,56 @@
+import com.atlassian.crowd.embedded.api.User
+import com.atlassian.jira.bc.issue.search.SearchService
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.user.util.UserUtil
+import com.atlassian.jira.web.bean.PagerFilter
+import com.atlassian.jira.issue.fields.CustomField
+import com.atlassian.jira.issue.CustomFieldManager
+
+jqlSearch = "'Epic Link' = " + issue.getKey() + " and issuetype != 'Core Crate Planning Task' and 'Locked In Product' = 'Yes'"
+SearchService searchService = ComponentAccessor.getComponent(SearchService.class)
+UserUtil userUtil = ComponentAccessor.getUserUtil()
+User user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+IssueManager issueManager = ComponentAccessor.getIssueManager()
+
+if (!user) {
+    user = userUtil.getUserObject('jira_bot')
+}
+
+List<Issue> issues = null
+
+SearchService.ParseResult parseResult = searchService.parseQuery(user, jqlSearch)
+if (parseResult.isValid())
+{
+    def searchResult = searchService.search(user, parseResult.getQuery(), PagerFilter.getUnlimitedFilter())
+    // Transform issues from DocumentIssueImpl to the "pure" form IssueImpl (some methods don't work with DocumentIssueImps)
+    issues = searchResult.issues.collect {issueManager.getIssueObject(it.id)}
+} else {
+    log.error("Invalid JQL: " + jqlSearch);
+}
+
+CustomFieldManager cfManager = ComponentAccessor.getComponent(CustomFieldManager)
+CustomField unitCost;
+List<CustomField> cFields;
+Double dValue;
+double totalUnitCost = 0
+for(int i=0; i < issues.size(); i++)
+{
+    Issue item = issues.get(i)
+    cFields = cfManager.getCustomFieldObjects(item)
+
+	for (int x = 0; x < cFields.size(); x++)
+	{
+    	if (cFields.get(x).getFieldName() == "Cost/Unit")
+    	{
+     	    unitCost = cFields.get(x)
+			dValue = (Double)item.getCustomFieldValue(unitCost)
+            if (dValue != null)
+           		totalUnitCost = totalUnitCost + dValue.doubleValue()
+        }
+ 	}
+    
+}
+return totalUnitCost
+
